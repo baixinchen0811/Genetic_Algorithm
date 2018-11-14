ea1f8177f42122ff3ee0c91ce93f81a936325610
baixinchen <34299647+baixinchen0811@users.noreply.github.com>
2018-11-14 22:34:39 +0800

    ##

--- ceshi.py
+++ ceshi.py
@@ -1,15 +1,12 @@
-# class Solution:
-#     """
-#     @param: chars: The letter array you should sort by Case
-#     @return: nothing
-#     """
-#
-# def sortLetters(self, chars):
-#     # write your code here
-#     chars.sort(key=lambda c: c.isupper())
-# sortLetters(chars)
-# chars = {'hhJJHKNjk'}
-# print(chars)
-s='asdf234GDSdsf23'
-x=sorted(s, key=lambda x: (x.isdigit(),x.isdigit() and int(x) % 2 == 0,x.isupper(),x.islower(),x))
+class Solution:
+    """
+    @param: chars: The letter array you should sort by Case
+    @return: nothing
+    """
+chars = 'hhJJHKNjk'
+x=sorted(chars,key=lambda c: c.isupper())
 print(x)
+
+# s='asdf234GDSdsf23'
+# x=sorted(s, key=lambda x: (x.isdigit(),x.isdigit() and int(x) % 2 == 0,x.isupper(),x.islower(),x))
+# print(x)
